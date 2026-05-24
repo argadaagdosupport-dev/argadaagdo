9c5c4ec0964d2af3ed7258280a7b25a3c06f964c
argadaagdosupport-dev <argadaagdo.support@gmail.com>
2026-05-25 01:22:57 +0400

    Delete components directory

--- components/QRCodeBox.tsx
+++
@@ -1,7 +0,0 @@
-export default function QRCodeBox() {
-  return (
-    <div className="mt-6 flex h-40 w-40 items-center justify-center rounded-3xl bg-gray-100 text-center text-sm font-black text-gray-500">
-      QR CODE
-    </div>
-  );
-}
--- components/StatCard.tsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value }: { title: string; value: string }) {
-  return (
-    <div className="rounded-3xl bg-white p-6 shadow-sm">
-      <p className="text-gray-500">{title}</p>
-      <p className="mt-2 text-4xl font-black text-green-700">{value}</p>
-    </div>
-  );
-}
